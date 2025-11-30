689f74e6155450e17b7985389adba6f6ce264f4a
belalanaskoura <153070932+belalanaskoura@users.noreply.github.com>
2025-11-30 17:34:45 +0200

    Update CreatePost.jsx
    Import fix

--- ReadIt/src/Components/pages/CreatePost.jsx
+++ ReadIt/src/Components/pages/CreatePost.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { CreatePost } from "../components/CreatePost";
+
+import CreatePost  from "../CreatePost/CreatePost/CreatePost";
 
 export default function CreatePostPage() {
   return (
