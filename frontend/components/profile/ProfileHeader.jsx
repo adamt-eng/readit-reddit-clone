e638dc088f13fdf283acbcb795b639e211bd55a8
nourannmohamed <nouranmohameda3@gmail.com>
2025-12-04 00:35:28 +0200

    Remove 'u/' prefix from profile username

--- frontend/components/profile/ProfileHeader.jsx
+++ frontend/components/profile/ProfileHeader.jsx
@@ -3,7 +3,7 @@ export default function ProfileHeader() {
   return (
     <div className="profile-header">
       <div className="profile-info">
-        <h1>u/Moist_Barber_9724</h1>
+        <h1>Moist_Barber_9724</h1>
         <p className="karma-line">Karma: 1 • Cake day: November 2024</p>
       </div>
     </div>
