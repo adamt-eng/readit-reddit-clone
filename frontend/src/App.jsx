d0b409599c1a598c545b6b57dda9d8ec71d06135
Adam <23P0001@eng.asu.edu.eg>
2025-12-03 20:36:08 +0200

    Move components to frontend directory and update imports
    Renamed and relocated component files from src/components to frontend/components. Updated App.jsx in frontend/src to import components from the new location. Removed the old src/App.jsx file.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import SearchResults from './pages/SearchResults/SearchResults'
+
+import DirectMessages from './components/DirectMessages'
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <Notifications></Notifications>
-  )
+    <>
+      <DirectMessages />
+    </>
+  );
 }
 
 export default App
