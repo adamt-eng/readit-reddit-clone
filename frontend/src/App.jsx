ff7cfbf0cac69cb6bf00ab21e32bb2debebebcb2
Omar Tamer <140625062+3omar146@users.noreply.github.com>
2025-12-03 11:47:38 +0200

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SearchResults from './pages/SearchResults/SearchResults'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <Notifications></Notifications>
+  )
+}
+
+export default App
