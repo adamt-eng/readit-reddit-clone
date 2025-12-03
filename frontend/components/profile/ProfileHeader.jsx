fd2e986f391a824196c33ecda7afb07f3388451f
nourannmohamed <nouranmohameda3@gmail.com>
2025-12-03 23:49:13 +0200

    Refactor ProfileHeader to remove user prop

--- frontend/components/profile/ProfileHeader.jsx
+++ frontend/components/profile/ProfileHeader.jsx
@@ -1,25 +1,4 @@
-// src/components/profile/ProfileHeader.jsx
-/*
-export default function ProfileHeader({ user }) {
-  return (
-    <div className="profile-header-card">
-      <div className="profile-header-content">
-        <div className="username-section">
-          <h1 className="display-name">u/{user.username}</h1>
-          <div className="karma-cake">
-            Karma: {user.karma}  •  Cake day: {user.cakeDay}
-          </div>
-        </div>
-        <div className="profile-actions">
-          <button className="btn-share">Share</button>
-          <button className="btn-edit">Edit Profile</button>
-        </div>
-      </div>
-    </div>
-  );
-}
-*/
-// src/components/profile/ProfileHeader.jsx
+
 export default function ProfileHeader() {
   return (
     <div className="profile-header">
