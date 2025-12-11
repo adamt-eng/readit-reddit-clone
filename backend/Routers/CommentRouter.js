72e243434db9c6a00935b286d0f8ff53d8335bd3
belalanaskoura <153070932+belalanaskoura@users.noreply.github.com>
2025-12-12 01:02:59 +0200

    Add reply to comment route in CommentRouter
    Introduced a new POST route '/:commentId/reply' to handle replies to comments by connecting it to the replyToComment controller.

--- backend/Routers/CommentRouter.js
+++ backend/Routers/CommentRouter.js
@@ -1,4 +1,13 @@
 import express from 'express';
 import auth from "../Middleware/AuthMiddleware.js";
+import { 
+    replyToComment
+ } from "../Controllers/CommentController.js";
+
+const router = express.Router();
+
+router.post("/:commentId/reply", replyToComment);
+
+
 
 export default router;
