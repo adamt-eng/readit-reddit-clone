3f04550b4caaca45040236cba45b12ed3b1b0105
nourannmohamed <nouranmohameda3@gmail.com>
2025-12-13 20:38:35 +0200

    Update ProfileHeader.jsx

--- frontend/components/profile/ProfileHeader.jsx
+++ frontend/components/profile/ProfileHeader.jsx
@@ -1,14 +1,18 @@
-import avatarImg from "../../assets/default-avatar.png";
+import defaultAvatar from "../../assets/default-avatar.png";
 
 export default function ProfileHeader({ user }) {
+  const avatarSrc = user?.avatarUrl
+    ? `http://localhost:5000${user.avatarUrl}`
+    : defaultAvatar;
+
   return (
     <div className="profile-header">
-      <img src={avatarImg} alt="avatar" className="header-avatar" />
+      <img src={avatarSrc} alt="avatar" className="header-avatar" />
 
       <div className="profile-info">
-        <h1>{user.username}</h1>
+        <h1>{user?.username}</h1>
         <p className="karma-line">
-          Karma: {user.karma} • Cake day: {user.cakeDay}
+          Karma: {user?.karma} • Cake day: {user?.cakeDay}
         </p>
       </div>
     </div>
