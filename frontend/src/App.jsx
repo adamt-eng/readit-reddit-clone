94859bdf34aefc965fc13ca215eabc85ec5fc9f2
Adam <23P0001@eng.asu.edu.eg>
2025-12-15 23:19:49 +0200

    Remove unused code and mock user logic from App.jsx
    Cleaned up App.jsx by removing commented-out mock user logic and unused imports. This streamlines the component and eliminates unnecessary code related to local testing and mock data.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState, useLayoutEffect, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar.jsx";
-import GuestHomePage from "../pages/GuestHomePage/GuestHomePage.jsx";
 import AppRoutes from "./routes.jsx";
 import AuthModal from "../pages/Authentication/AuthModal.jsx";
 import "./App.css";
 import axios from "axios"
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [, setIsLoggedIn] = useState(false);
   const [currentUser, setCurrentUser] = useState(null);
   const [darkMode, setDarkMode] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
@@ -22,47 +21,6 @@ function App() {
 
   const location = useLocation();
   const isAuthPage = location.pathname === "/login" || location.pathname === "/signup";
-
-  /* const mockUser = {
-    _id: "693c92f1c4258e79913cd1d7", // put a REAL ID from MongoDB Compass
-    username: "john_doe",
-    avatarUrl: "", // backend uses avatarUrl
-    karma: 1247,
-  }; */
-
-  // Try to load a real user from the backend for local testing
-  // for DM testing -- adam
-  // DMS WILL ONLY WORK EITHER WITH THIS OR WITH AUTH!!!!!!!!!
-/*   useEffect(() => {
-    async function loadUser() {
-      try {
-        const res = await axios.get(
-          "http://localhost:5000/users/search?q=b&page=1&limit=1"
-        );
-        const users = res.data && res.data.results ? res.data.results : [];
-        if (users.length) {
-          setCurrentUser(users[0]);
-          setIsLoggedIn(true);
-        } else {
-          setCurrentUser(mockUser);
-        }
-
-        const currentPort = window.location.port;
-        console.log(`Attempting to load user from backend to port number ${currentPort ?? "aaa"}...`);
-        
-      } catch (err) {
-        console.error("Failed to load user from backend, falling back to mock:", err);
-        setCurrentUser(mockUser);
-      }
-    }
-
-    loadUser();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); */
-
- 
-
-
   
 useEffect(() => {
   const fetchMe = async () => {
@@ -102,7 +60,7 @@ useEffect(() => {
       });
       setCurrentUser(res.data);
       setIsLoggedIn(true);
-    } catch (err) {
+    } catch {
       setCurrentUser(null);
       setIsLoggedIn(false);
     } finally {
