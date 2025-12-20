8b4c6286ad563f5f036844175911857ab8efef53
nourannmohamed <nouranmohameda3@gmail.com>
2025-12-20 13:28:22 +0200

    rwmove unused variable

--- frontend/components/profile/ProfileTabs.jsx
+++ frontend/components/profile/ProfileTabs.jsx
@@ -1,5 +1,4 @@
-const tabs = ["Posts", "Communities", "Comments","Saved"];
-
+//const tabs = ["Posts", "Communities", "Comments","Saved"];
 export default function ProfileTabs({isMyProfile, activeTab, setActiveTab }) {
   const tabs = isMyProfile?["Posts", "Communities", "Comments","Saved"]:["Posts", "Communities", "Comments"];
   return (
